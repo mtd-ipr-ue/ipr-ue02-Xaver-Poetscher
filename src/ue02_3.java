452899d95fd105002f1b6d06e6f52e4cbc5db957
Xaver Pötscher <S2410238045@fhooe.at>
2024-10-28 17:51:52 +0100

    Tried boolean ue02_3 and it worked

--- src/ue02_3.java
+++ src/ue02_3.java
@@ -1,7 +1,21 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_3 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
-    }
+        Out.println("Please enter x coordinate");
+        double x = In.readDouble();
+
+        Out.println("Please enter y coordinate");
+        double y = In.readDouble();
+
+        boolean greenArea = (x>= 0 && y>=0) && ( x*x +y*y >= 4*4) && (x<=4 && y<=4);
+        
+        if (greenArea) {
+            System.out.println("The Point is inside of the green Area");
+        }
+        else {
+            System.out.println("The Point is outside of the green Area");
+        }
+    }  
 }
