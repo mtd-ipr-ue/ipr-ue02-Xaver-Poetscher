c566d9b85a2856236701d611daf796597d39a5fb
Xaver Pötscher <S2410238045@fhooe.at>
2024-10-28 17:09:48 +0100

    Completed UE02_1

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -1,7 +1,25 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_1 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
-    }
+
+        Out.println("Please enter first number");
+        int Number1 = In.readInt();
+
+        Out.println("Please enter second number");
+        int Number2 = In.readInt();
+
+        Out.println("Please enter third number");
+        int Number3 = In.readInt();
+
+        int max = Number1;
+        if (max < Number2) {
+            max = Number2;
+        }
+        if (max < Number3) {   
+            max = Number3;
+        }
+        System.out.println("The maximum is "+max);
+    } 
 }
